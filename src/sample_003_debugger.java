54bf06bbe9858430101a2470bbc35684d3851c76
yuk-youngmin <a84146943@gmail.com>
2022-04-06 23:58:55 +0900

    디버거 실습 코드

---
+++ src/sample_003_debugger.java
@@ -0,0 +1,14 @@
+public class sample_003_debugger {
+    public static void main(String[] args)
+    {
+        String id1 = "ID 1 VALUE";
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        String id2 = "ID 2 VALUE";
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("F");
+        System.out.println("G");
+    }
+}
