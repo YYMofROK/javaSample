d1f128efb6f07689431b4e92babb4fe214784d1d
yuk-youngmin <a84146943@gmail.com>
2022-04-06 23:59:33 +0900

    팝업 - 다이알로그 입력 창 실습

---
+++ src/sample_004_input_output.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class sample_004_input_output {
+    public static void main(String[] args) {
+
+        String id1 = JOptionPane.showInputDialog("Enter a id1");;
+
+        System.out.println( id1 );
+
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("F");
+        System.out.println("G");
+    }
+}
