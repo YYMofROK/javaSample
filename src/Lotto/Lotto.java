86ec6d01ec30d3d49b407bd2f9e2a441b5525dfc
yuk-youngmin <a84146943@gmail.com>
2022-05-26 21:28:55 +0900

    결과 출력 텍스트 수정

--- src/Lotto/Lotto.java
+++ src/Lotto/Lotto.java
@@ -13,7 +13,7 @@ public class Lotto {
         int lottoCnt = computer.checkLotto(user);
         System.out.println("사용자 입력 숫자 : " + user.getNumber());
         System.out.println("컴퓨터 생성 숫자 : " + computer.getNumber());
-        System.out.println(lottoCnt);
+        System.out.println("당첨 숫자 : " + lottoCnt);
 
     }
 
